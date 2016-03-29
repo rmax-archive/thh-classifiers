974005f9f5e8a1deb32a1c51a9287568aac90013
zuev <zuev@scrapinghub.com>
2016-03-29 20:30:55 +0300

    fb spiders update

--- dirbot/dirbot/spiders/facebook.py
+++ dirbot/dirbot/spiders/facebook.py
@@ -14,7 +14,7 @@ API_ENDPOINT = "https://graph.facebook.com/v2.5/"
 API_TOKEN_ENDPOINT = "https://graph.facebook.com/v2.5/oauth/access_token"
 
 _FB_SEARCH_FIELDS = [
-    'id', 'name',
+    'id', 'name', 'age_range', 'bio', 'gender', 'website', 'work', 'link'
 ]
 
 
@@ -22,6 +22,7 @@ class FbSearchItem(Item):
     id = Field()
     name = Field()
     keywords = Field()
+    userinfo = Field()
 
 
 class FbGraphSearchSpider(Spider):
@@ -29,6 +30,8 @@ class FbGraphSearchSpider(Spider):
     query = "Egorov"
     # query delay in seconds
     next_page_delay = 10
+    follow_next = False
+    handle_httpstatus_list = [400, 404, 401, 403, 301, 302, 500, 520, 504]
 
     def get_request(self, query=None,
                     obj_type="user",
@@ -67,19 +70,43 @@ class FbGraphSearchSpider(Spider):
         # self.log(data,
         #         level=logging.INFO
         # )
+        formdata = {
+            'access_token': settings.FB_TOKEN,
+            'fields': ",".join(_FB_SEARCH_FIELDS),
+        }
         for search_res in data['data']:
-            item = FbSearchItem()
-            item["id"] = search_res["id"]
-            item["name"] = search_res["name"]
-            item["keywords"] = kw
-            yield item
+            yield FormRequest(
+                API_ENDPOINT + search_res["id"],
+                method="GET",
+                callback=self.parse_user,
+                formdata=dict((k, v) for k, v in formdata.iteritems() if v),
+                meta={}
+            )
 
-        if 'paging' in data and 'next' in data['paging']:
-            yield Request(data['paging']['next'],
-                          dont_filter=True,
-                          meta={"keywords": kw,
-                                'delay_request': self.next_page_delay,
-                          })
+
+        if self.follow_next:
+            if 'paging' in data and 'next' in data['paging']:
+                yield Request(data['paging']['next'],
+                              dont_filter=True,
+                              meta={"keywords": kw,
+                                    'delay_request': self.next_page_delay,
+                              })
+
+    def parse_user(self, response):
+        if response.status != 200:
+            self.log(
+                'Response %s has a non-200 status with body: %s'
+                % (str(response), response.body),
+                level=logging.WARNING
+            )
+            return
+        data = json.loads(response.body)
+        self.log(data,level=logging.INFO)
+        item = FbSearchItem()
+        item["id"] = data["id"]
+        item["name"] = data["name"]
+        item["userinfo"] = data
+        yield item
 
 
 class FbLongLiveTokenSpider(Spider):
---
+++ dirbot/dirbot/spiders/fb2.py
@@ -0,0 +1,96 @@
+import logging
+import csv
+import cStringIO
+import re
+import random
+from datetime import datetime
+
+from scrapy import log
+from scrapy.conf import settings
+from scrapy.http import FormRequest, Request
+from scrapy.selector import Selector
+from scrapy.spider import Spider
+
+
+class FB2Spider(Spider):
+    name = 'fb2'
+    account = {}
+
+    def start_requests(self):
+        self.account['email'] = "nikaskriabina@gmail.com"
+        self.account['password'] = "F,hfrflf,hf2016"
+        # dob = "1990.01.01"
+        self.account['year'] = "1990"
+        self.account['month'] = "1"
+        self.account['day'] = "1"
+        yield Request('https://www.facebook.com', callback=self.log_in)
+
+    def log_in(self, response):
+        # set current account
+        self.current_account = self.account
+        logging.debug('Logging in with: %s' % self.current_account['email'])
+        return FormRequest.from_response(
+            response,
+            formdata={'email': self.current_account['email'],
+                      'pass': self.current_account['password']},
+            callback=self.check_log_in,
+            dont_filter=True
+        )
+
+    def check_log_in(self, response):
+        if 'checkpoint' in response.url:
+            return FormRequest.from_response(
+                response,
+                formdata={'submit[Continue]': 'Continue'},
+                callback=self.birthday_captcha,
+                dont_filter=True
+            )
+        else:
+            return self.login_finished(response)
+
+    def birthday_captcha(self, response):
+        return FormRequest.from_response(
+            response,
+            formdata={'birthday_captcha_month': self.current_account['month'],
+                      'birthday_captcha_day': self.current_account['day'],
+                      'birthday_captcha_year': self.current_account['year'],
+                      'submit[Continue]': 'Continue'},
+            callback=self.confirm_location,
+            dont_filter=True
+        )
+
+    def confirm_location(self, response):
+        if 'checkpoint' in response.url:
+            return FormRequest.from_response(
+                response,
+                formdata={'submit[This was me]': 'This was me'},
+                callback=self.login_finished,
+                dont_filter=True
+            )
+        else:
+            return self.login_finished(response)
+
+    def login_finished(self, response):
+        facebook_ids = ["100001665518840", "100000118429707"]
+        reqs = []
+        for facebook_id in facebook_ids:
+            reqs.append(Request('https://www.facebook.com/%s' % facebook_id,
+                          callback=self.parse_profile,
+                          meta={"id":facebook_id}))
+        return reqs
+
+    def parse_profile(self, response):
+        return Request(response.url + '/info',
+                       callback=self.parse_about,
+                       meta={"id":response.meta["id"]})
+
+    def parse_about(self, response):
+        with open("/media/sf_temp/{}.html".format(response.meta["id"]), "w") as fout:
+            fout.write(response.body)
+        sel = Selector(response)
+        logging.info("name = {}".format(sel.xpath('//title').extract()))
+        for comment in sel.xpath('//*[not(self::script)]/comment()'):
+            comment_string = u''.join(comment.extract()).strip()
+            m = re.compile(r'<!..(.+)..>', re.UNICODE).search(comment_string)
+            if m:
+                logging.info(m.group(1).strip())
